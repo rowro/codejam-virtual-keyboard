63953ca8729a7ad7e30a046a3c42215e5f90da12
rowro <rowro.dev@gmail.com>
2019-11-04 17:10:23 +0300

    refactor: apply eslint and refactor code

--- src/Key.js
+++ src/Key.js
@@ -1,64 +1,62 @@
 export default class Key {
-    constructor({ key, code, shiftKey = null, size = null, isControlKey = false }, language, capsLock = false) {
-        this.el = null;
-        this.key = key;
-        this.code = code;
-        this.shiftKey = shiftKey;
-        this.size = size;
-        this.isControlKey = isControlKey;
-        this.language = language;
-        this.capsLock = capsLock;
+  constructor({
+    key, code, shiftKey = null, size = null, isControlKey = false,
+  }, language) {
+    this.el = null;
+    this.key = key;
+    this.code = code;
+    this.shiftKey = shiftKey;
+    this.size = size;
+    this.isControlKey = isControlKey;
+    this.language = language;
+    this.isUpperCase = false;
 
-        this.init();
+    this.init();
+  }
+
+  get label() {
+    const label = (this.key.ru && this.key.en) ? this.key[this.language] : this.key;
+    return (this.isUpperCase && !this.isControlKey) ? label.toUpperCase() : label;
+  }
+
+  get shiftKeyLabel() {
+    return (this.shiftKey.ru && this.shiftKey.en) ? this.shiftKey[this.language] : this.shiftKey;
+  }
+
+  init() {
+    this.el = document.createElement('div');
+    this.el.className = 'key';
+
+    if (this.size) {
+      this.el.classList.add(`key_size_${this.size}`);
     }
 
-    get label() {
-        let label = (this.key.ru && this.key.en) ? this.key[this.language] : this.key;
-        return (this.capsLock && !this.isControlKey) ? label.toUpperCase() : label;
+    this.el.dataset.code = this.code;
+
+    this.render();
+  }
+
+  render() {
+    if (this.shiftKey) {
+      this.el.dataset.shiftKey = this.shiftKeyLabel;
     }
-    get shiftKeyLabel() {
-        return (this.shiftKey.ru && this.shiftKey.en) ? this.shiftKey[this.language] : this.shiftKey;
-    }
+    this.el.innerText = this.label;
+  }
 
-    init() {
-        this.el = document.createElement('div');
-        this.el.className = 'key';
+  setActive() {
+    this.el.classList.add('key_active');
+    setTimeout(() => {
+      this.el.classList.remove('key_active');
+    }, 300);
+  }
 
-        if (this.size) {
-            this.el.classList.add(`key_size_${this.size}`);
-        }
+  setLanguage(lang) {
+    this.language = lang;
+    this.render();
+  }
 
-        this.el.dataset.code = this.code;
-
-        this.render();
-    }
-
-    render() {
-        if (this.shiftKey) {
-            this.el.dataset.shiftKey = this.shiftKeyLabel;
-        }
-        this.el.innerText = this.label;
-    }
-
-    setActive() {
-        this.el.classList.add('key_active');
-        setTimeout(() => {
-            this.el.classList.remove('key_active');
-        }, 300)
-    }
-
-
-    setLanguage(lang) {
-        this.language = lang;
-        this.render();
-    }
-
-    toggleCapsLock(once = false) {
-        this.capsLock = !this.capsLock;
-        this.render();
-
-        if (once) {
-            this.capsLock = false;
-        }
-    }
+  toggleCase() {
+    this.isUpperCase = !this.isUpperCase;
+    this.render();
+  }
 }
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,113 +1,153 @@
-import Key from "./Key";
+import Key from './Key';
 
 export default class Keyboard {
-    constructor(keyboardTemplate) {
-        this.language = 'ru';
-        this.capsLock = false;
-        this.keyboardTemplate = keyboardTemplate;
-        this.el = null;
-        this.field = null;
-        this.keys = [];
+  constructor(keyboardTemplate) {
+    this.language = 'ru';
+    this.altKeyClicked = false;
+    this.shiftKeyClicked = false;
+    this.keyboardTemplate = keyboardTemplate;
+    this.el = null;
+    this.field = null;
+    this.keys = [];
+  }
+
+  init() {
+    // Load language from localstorage or save default language
+    if (localStorage.getItem('language')) {
+      this.language = localStorage.getItem('language');
+    } else {
+      localStorage.setItem('language', this.language);
     }
 
-    init() {
-        if (localStorage.getItem('language')) {
-            this.language = localStorage.getItem('language');
+    this.render();
+
+    // On click virtual key
+    document.addEventListener('click', (e) => {
+      const key = this.keys.find((item) => item.el === e.target);
+      if (key) {
+        if (key.key === 'Shift') {
+          // Change language on click on virtual keyboard
+          if (this.altKeyClicked) {
+            this.changeLanguage();
+          }
+
+          // Change letter case on click 'Shift' key
+          this.shiftKeyClicked = !this.shiftKeyClicked;
+          this.toggleCapsLock();
+        }
+
+        this.triggerKey(key, e);
+
+        if (key.key !== 'Alt' && this.altKeyClicked) {
+          this.altKeyClicked = false;
+        }
+
+        if (key.key !== 'Shift' && this.shiftKeyClicked) {
+          this.shiftKeyClicked = false;
+          this.toggleCapsLock();
+        }
+      }
+    });
+
+    // On press device key
+    document.addEventListener('keyup', (e) => {
+      const key = this.keys.find((item) => item.code === e.code);
+      if (key) {
+        this.triggerKey(key, e);
+      }
+    });
+  }
+
+  render() {
+    // Create field
+    this.field = document.createElement('textarea');
+    this.field.className = 'field';
+    this.field.disabled = 'disabled';
+    document.body.append(this.field);
+
+    // Create keyboard
+    this.el = document.createElement('div');
+    this.el.className = 'keyboard';
+
+    // Create keyboard keys
+    this.keyboardTemplate.forEach((row, rowIndex) => {
+      row.forEach((item) => {
+        const key = new Key(item, this.language);
+        key.el.classList.add(`key_row_${rowIndex + 1}`);
+
+        this.el.append(key.el);
+        this.keys.push(key);
+      });
+    });
+
+    document.body.append(this.el);
+  }
+
+  changeLanguage() {
+    this.language = (this.language === 'ru') ? 'en' : 'ru';
+    localStorage.setItem('language', this.language);
+    this.keys.forEach((item) => item.setLanguage(this.language));
+  }
+
+  toggleCapsLock() {
+    this.keys.forEach((item) => item.toggleCase());
+  }
+
+  triggerKey(key, e = null) {
+    this.field.blur();
+    key.setActive();
+
+    switch (key.code) {
+      case 'Backspace':
+        this.field.value = this.field.value.slice(0, -1);
+        break;
+
+      case 'CapsLock':
+        this.toggleCapsLock();
+        break;
+
+      case 'Enter':
+        this.field.value += '\n';
+        break;
+
+      case 'Space':
+        this.field.value += ' ';
+        break;
+
+      case 'AltLeft':
+      case 'AltRight':
+        if (e.shiftKey) {
+          this.changeLanguage();
         } else {
-            localStorage.setItem('language', this.language);
+          this.altKeyClicked = true;
+        }
+        break;
+
+      // Unused keys
+      case 'Tab':
+      case 'Delete':
+      case 'MetaLeft':
+      case 'MetaRight':
+      case 'ShiftLeft':
+      case 'ShiftRight':
+      case 'ControlLeft':
+      case 'ControlRight':
+        break;
+
+      default: {
+        // If key has shiftKey - like a '1' when shift pressed be '!', 2 - @, etc
+        const hasShiftKey = (e.shiftKey || this.shiftKeyClicked) && key.shiftKey;
+        const label = (hasShiftKey) ? key.shiftKeyLabel : key.el.innerText;
+
+        // If pressed 'Shift' key on physical keyboard
+        if (e.shiftKey) {
+          this.field.value += label.toUpperCase();
+        } else {
+          this.field.value += label;
         }
 
-        this.render();
-
-        document.addEventListener('click', (e) => {
-            let key = this.keys.find(item => item.el === e.target);
-            if (key) {
-                this.triggerKey(key, e);
-            }
-        });
-
-        document.addEventListener('keyup', (e) => {
-            let key = this.keys.find(item => item.code === e.code);
-            if (key) {
-                this.triggerKey(key, e);
-            }
-        });
+        break;
+      }
     }
-
-    render() {
-        this.field = document.createElement('textarea');
-        this.field.className = 'field';
-        this.field.disabled = 'disabled';
-        document.body.append(this.field);
-
-        this.el = document.createElement('div');
-        this.el.className = 'keyboard';
-
-        this.keyboardTemplate.forEach((row, rowIndex) => {
-            row.forEach((item) => {
-                let key = new Key(item, this.language, this.capsLock);
-                key.el.classList.add(`key_row_${rowIndex + 1}`);
-
-                this.el.append(key.el);
-                this.keys.push(key);
-            });
-        });
-
-        document.body.append(this.el);
-    }
-
-    changeLanguage() {
-        this.language = (this.language === 'ru') ? 'en' : 'ru';
-        localStorage.setItem('language', this.language);
-        this.keys.forEach(item => item.setLanguage(this.language));
-    }
-
-    triggerKey(key, e = null) {
-        this.field.blur();
-        key.setActive();
-
-        switch (key.code) {
-            case 'Backspace':
-                this.field.innerHTML = this.field.value.slice(0, -1);
-                break;
-
-            case 'CapsLock':
-                this.capsLock = !this.capsLock;
-                this.keys.forEach(item => item.toggleCapsLock());
-                break;
-
-            case 'Enter':
-                this.field.value += '\n';
-                break;
-
-            case 'Space':
-                this.field.value += '&#32;';
-                break;
-
-            case 'AltLeft':
-            case 'AltRight':
-                if (e.shiftKey) {
-                    this.changeLanguage();
-                }
-                break;
-
-            case 'Tab':
-            case 'Delete':
-            case 'MetaLeft':
-            case 'MetaRight':
-            case 'ShiftLeft':
-            case 'ShiftRight':
-            case 'ControlLeft':
-            case 'ControlRight':
-                break;
-
-            default:
-                if (e.shiftKey) key.toggleCapsLock();
-
-                this.field.value += ((e.shiftKey) && key.shiftKey) ? key.shiftKeyLabel : key.el.innerText;
-
-                if (e.shiftKey) key.toggleCapsLock();
-                break;
-        }
-    }
+  }
 }
